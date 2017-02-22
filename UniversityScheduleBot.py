65bfdcbf8f2b4c72b005e84bfe91555950ad1900
paladin-705 <paladin705@yandex.ru>
2017-02-23 00:35:27 +0300

    Немного маленьких изменений

--- UniversityScheduleBot.py
+++ UniversityScheduleBot.py
@@ -4,15 +4,14 @@ from telebot import types
 import time
 
 import sqlite3
+import re
 import hashlib
 
 import config
 
 commands = {  # command description used in the "help" command
               'start': 'Get used to the bot',
-              'help': 'Gives you information about the available commands',
-              'sendLongText': 'A test using the \'send_chat_action\' command',
-              'getImage': 'A test using multi-stage messages, custom keyboard, and media sending'
+              'help': 'Gives you information about the available commands'
 }
 
 def createSchedule(tag, day, type):
@@ -31,6 +30,75 @@ dateSelect.row("Понедельник","Вторник")
 dateSelect.row("Среда","Четверг")
 dateSelect.row("Пятница","Суббота")
 
+
+#registration
+@bot.callback_query_handler(func=lambda call: True)
+def callback_registration(call):
+    callbackData = re.split(r':', call.data)
+    callType = callbackData[0]
+    
+    if callType == "registration":
+        registration(call.data, call.message.chat.id, call.message.chat.first_name, call.message.chat.username)
+
+def registration(data, cid, name, username):
+    #parse message
+    #stage : org : fac : gr 
+    callbackData = re.split(r':', data)
+    stage = callbackData[1]
+
+    try:
+        con = sqlite3.connect('base/base.db')
+        cur = con.cursor()
+    except sqlite3.Error:
+        sys.exit(1)
+    
+    if stage == "stage 1":
+        keyboard = types.InlineKeyboardMarkup()
+        
+        cur.execute('SELECT DISTINCT organization FROM organizations')
+        result = cur.fetchall()
+        for row in result:
+            callback_button = types.InlineKeyboardButton(text=str(row[0]), callback_data="registration:stage 2:{0}".format(str(row[0])))
+            keyboard.add(callback_button)
+        
+        bot.send_message(cid, "Выберите университет:", reply_markup=keyboard)
+    elif stage == "stage 2":
+        organization = callbackData[2]
+        keyboard = types.InlineKeyboardMarkup()
+        
+        cur.execute('SELECT DISTINCT faculty FROM organizations WHERE organization=(?)', [organization])
+        result = cur.fetchall()
+        for row in result:
+            callback_button = types.InlineKeyboardButton(text=str(row[0]), callback_data="registration:stage 3:{0}:{1}".format(organization, str(row[0])))
+            keyboard.add(callback_button)
+        
+        bot.send_message(cid, "Выберите факультет:", reply_markup=keyboard)
+        
+    elif stage == "stage 3":
+        organization = callbackData[2]
+        faculty = callbackData[3]
+        keyboard = types.InlineKeyboardMarkup()
+        cur.execute('SELECT id, studGroup FROM organizations WHERE organization=(?) AND faculty=(?)', [organization, faculty])
+        result = cur.fetchall()
+        for row in result:
+            callback_button = types.InlineKeyboardButton(text=str(row[1]), callback_data="registration:stage 4:{0}".format(str(row[0])))
+            keyboard.add(callback_button)
+        
+        bot.send_message(cid, "Выберите группу:", reply_markup=keyboard)
+    elif stage == "stage 4":
+        groupId = callbackData[2]
+        
+        cur.execute('SELECT tag FROM organizations WHERE id=(?)', [groupId])
+        row = cur.fetchall()
+        
+        cur.execute('INSERT INTO users VALUES(?,?,?,?)',(cid, name, username, str(row[0][0])))
+        con.commit()
+        con.close()
+        bot.send_message(cid, "Отлично, вы зарегистрировались", reply_markup=dateSelect)
+    else:
+        print("foobar")
+    
+    
 # handle the "/start" command
 @bot.message_handler(commands=['start'])
 def command_start(m):
@@ -49,13 +117,10 @@ def command_start(m):
     if user:
         bot.send_message(cid, "Вы уже добавлены в базу данных", reply_markup=dateSelect)
     else:
-        groupSelect = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
-        groupSelect.row("1 Группа", "2 Группа")
-
-        cur.execute('INSERT INTO users VALUES(?,?,?,?)',(cid, m.chat.first_name, m.chat.username, ''))
-        con.commit()
-        con.close()
-        bot.send_message(cid, "Отлично! Теперь выберите подгруппу:", reply_markup=groupSelect)
+        #groupSelect = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
+        #groupSelect.row("1 Группа", "2 Группа")
+        registration("registration:stage 1: none", cid, m.chat.first_name, m.chat.username)
+        
         #command_help(m)
 
 # help page
@@ -67,14 +132,6 @@ def command_help(m):
         help_text += "/" + key + ": "
         help_text += commands[key] + "\n"
     bot.send_message(cid, help_text, reply_markup=dateSelect)  # send the generated help page
-
-
-# user can chose an image (multi-stage command example)
-@bot.message_handler(commands=['getImage'])
-def command_image(m):
-    cid = m.chat.id
-    bot.send_message(cid, "Please choose your image now", reply_markup=dateSelect)  # show the keyboard
-    userStep[cid] = 1  # set the user to the next step (expecting a reply in the listener now)
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def response_msg(m):
@@ -100,6 +157,6 @@ def response_msg(m):
                 bot.send_message(m.chat.id, "1 - Инженерная и компьютерная графика лаб. 1-131\n 2 - Неорган. химия лаб. 1-131\n 3 - Неорган. химия лаб. 3-455", reply_markup=dateSelect)
     else:
         bot.send_message(m.chat.id, "Неизвестная команда", reply_markup=dateSelect)
-
+            
 bot.polling()
 
--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ import sqlite3
 try:
 	con = sqlite3.connect('base/base.db')
 	cur = con.cursor()
-	cur.execute("CREATE TABLE users(id INT, name TEXT, username TEXT, hash TEXT);")
-	cur.execute("CREATE TABLE schedule(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, tag TEXT, day TEXT, number INTEGER, type TEXT, startTime TEXT, endTime TEXT, title TEXT, classroom TEXT, lecturer TEXT);")
+	cur.execute("CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT, username TEXT, scheduleTag TEXT);")
+	cur.execute("CREATE TABLE organizations(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, organization TEXT, faculty TEXT, studGroup TEXT, tag TEXT);")
+	cur.execute("CREATE TABLE schedule(tag TEXT PRIMARY KEY NOT NULL, day TEXT, number INTEGER, type TEXT, startTime TEXT, endTime TEXT, title TEXT, classroom TEXT, lecturer TEXT);")
 except sqlite3.Error:
 	print("Oops! some errors")
 finally:
