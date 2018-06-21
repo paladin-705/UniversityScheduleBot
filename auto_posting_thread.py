500794282938320db78c7d77983951a22feddd19
paladin-705 <paladin705@yandex.ru>
2018-06-21 16:07:47 +0300

    Рефакторинг

--- auto_posting_thread.py
+++ auto_posting_thread.py
@@ -23,7 +23,33 @@ logging.basicConfig(format='%(asctime)-15s [ %(levelname)s ] uid=%(userid)s %(me
 logger = logging.getLogger('bot-logger')
 
 
-def auto_posting(current_time):
+def auto_posting(current_time, day, week_type, is_today=True):
+    # Выборка пользователей из базы
+    with ScheduleDB(config) as db:
+        users = db.find_users_where(auto_posting_time=current_time, is_today=is_today)
+
+    if users is None:
+        return None
+    try:
+        for user in users:
+            cid = user[0]
+            tag = user[1]
+
+            schedule = create_schedule_text(tag, day[0], week_type)
+            if len(schedule[0]) <= 14:
+                continue
+            bot.send_message(cid, schedule, reply_markup=get_date_keyboard())
+
+            # Статистика
+            if config['STATISTIC_TOKEN'] != '':
+                track(config['STATISTIC_TOKEN'], cid, current_time, 'auto_posting')
+            else:
+                logger.info('auto_posting. Time: {0}'.format(current_time), extra={'userid': cid})
+    except BaseException as e:
+        logger.warning('auto_posting: {0}'.format(str(e)), extra={'userid': 0})
+
+
+def today_schedule(current_time):
     today = datetime.now()
     week_type = get_week_type(today)
 
@@ -33,64 +59,33 @@ def auto_posting(current_time):
 
     day = [daysOfWeek[datetime.weekday(today)]]
 
-    # Выборка пользователей из базы у которых установлена отправка расписния на текущий день
-    with ScheduleDB(config) as db:
-        users = db.find_users_where(auto_posting_time=current_time, is_today=True)
+    auto_posting(current_time, day, week_type)
 
-    if users is None:
-        return None
-    try:
-        for user in users:
-            cid = user[0]
-            tag = user[1]
 
-            schedule = create_schedule_text(tag, day[0], week_type)
-            if len(schedule[0]) <= 14:
-                continue
-            bot.send_message(cid, schedule, reply_markup=get_date_keyboard())
-
-            # Статистика
-            if config['STATISTIC_TOKEN'] != '':
-                track(config['STATISTIC_TOKEN'], cid, current_time, 'auto_posting')
-            else:
-                logger.info('auto_posting. Time: {0}'.format(current_time), extra={'userid': cid})
-    except BaseException as e:
-        logger.warning('auto_posting: {0}'.format(str(e)), extra={'userid': 0})
+def tomorrow_schedule(current_time):
+    tomorrow = datetime.now()
+    tomorrow += timedelta(days=1)
+    week_type = get_week_type(tomorrow)
 
     # Выборка пользователей из базы у которых установлена отправка расписния на завтрашний день,
     # если сегодня воскресенье, то расписание будет отправляться на понедельник.
-    if datetime.weekday(datetime.now()) != 6:
-        today += timedelta(days=1)
+    if datetime.weekday(tomorrow) == 6:
+        tomorrow += timedelta(days=1)
+        week_type = (week_type + 1) % 2
 
-    day = [daysOfWeek[datetime.weekday(today)]]
+    day = [daysOfWeek[datetime.weekday(week_type)]]
 
-    with ScheduleDB(config) as db:
-        users = db.find_users_where(auto_posting_time=current_time, is_today=False)
-
-    if users is None:
-        return None
-    try:
-        for user in users:
-            cid = user[0]
-            tag = user[1]
-
-            schedule = create_schedule_text(tag, day[0], week_type)
-            if len(schedule[0]) <= 14:
-                continue
-            bot.send_message(cid, schedule, reply_markup=get_date_keyboard())
-
-            # Статистика
-            if config['STATISTIC_TOKEN'] != '':
-                track(config['STATISTIC_TOKEN'], cid, current_time, 'auto_posting')
-            else:
-                logger.info('auto_posting. Time: {0}'.format(current_time), extra={'userid': cid})
-    except BaseException as e:
-        logger.warning('auto_posting: {0}'.format(str(e)), extra={'userid': 0})
+    auto_posting(current_time, day, week_type, is_today=False)
 
 
 if __name__ == "__main__":
     while True:
-        threading.Thread(target=auto_posting(datetime.now().time().strftime("%H:%M:00"))).start()
+        # Отправка расписания на сегодня
+        threading.Thread(target=today_schedule(datetime.now().time().strftime("%H:%M:00"))).start()
+
+        # Отправка расписания на завтра
+        threading.Thread(target=tomorrow_schedule(datetime.now().time().strftime("%H:%M:00"))).start()
+
         # Вычисляем разницу в секундах, между началом минуты и временем завершения потока
         time_delta = datetime.now() - datetime.now().replace(second=0, microsecond=0)
         # Поток засыпает на время равное количеству секунд до следующей минуты
