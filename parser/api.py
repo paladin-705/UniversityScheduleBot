7bc9c34e40fd7f6976e60716546a511ea534bbe6
Kornev Sergey <paladin705@yandex.ru>
2018-07-20 22:00:18 +0300

    Update api.py

--- parser/api.py
+++ parser/api.py
@@ -20,12 +20,11 @@ def add_organization(organization, faculty, group):
          }
     )
     try:
-        res = requests.post(url + '/api/organization', json=data, verify=False)
+        res = requests.post(url + '/api/organization', json=data, verify=False, timeout=2*60)
         answer = json.loads(res.text)
     except:
         with open('failed-organization.txt', "a") as file:
-            for d in answer['failed']:
-                file.write(str(d['data']) + '\n')
+            file.write(str(data['data']) + '\n')
         return None
 
     if len(answer['failed']) != 0:
@@ -59,12 +58,11 @@ def add_lesson(tag, day, number, week_type, time_start, time_end, title, classro
          }
     )
     try:
-        res = requests.post(url + '/api/schedule', json=data, verify=False)
+        res = requests.post(url + '/api/schedule', json=data, verify=False, timeout=2*60)
         answer = json.loads(res.text)
     except:
         with open('failed-schedule.txt', "a") as file:
-            for d in answer['failed']:
-                file.write(str(d['data']) + '\n')
+            file.write(str(data['data']) + '\n')
         return False
 
     if len(answer['failed']) != 0:
