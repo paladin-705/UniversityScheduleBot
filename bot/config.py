4fb0a22110630472bbda54a3c50ef016817121f9
paladin-705 <paladin705@yandex.ru>
2021-02-27 02:23:02 +0300

    Fix

--- bot/config.py
+++ bot/config.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 import configparser
-import os
 
 config_file = configparser.ConfigParser()
 config = config_file['DEFAULT']
 
-current_path = os.path.abspath(os.path.dirname(__file__))
-config_file.read(current_path + '/' + "config.ini")
+config_file.read('config.ini')
