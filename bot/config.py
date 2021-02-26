cb5c09c38be064f8520f7dc2ebb3fc7085656271
paladin-705 <paladin705@yandex.ru>
2021-02-27 02:46:50 +0300

    Fix

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import configparser
+import os
 
 config_file = configparser.ConfigParser()
 config = config_file['DEFAULT']
 
-config_file.read('config.ini')
+current_path = os.path.abspath(os.path.dirname(__file__))
+config_file.read(current_path + '/' + "config.ini")
